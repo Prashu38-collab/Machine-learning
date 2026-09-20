8d4f0745d9cf01c7aed9a740cf6831de1015e0d0
Prashamsa Ghimire <ghimireprashamsa26@gmail.com>
2026-09-20 10:29:05 +0545

    vector addition and scalar multiplication

---
+++ vectors/vectoradd.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array1=np.array([1,2,3])
+array2=np.array([4,5,6])
+# vector addition
+result=array1+array2
+print("Vector addition result:", result)
+# scalar multiplication
+scalar=2
+result_final=scalar*result
+print("Scalar multiplication result:", result_final)
